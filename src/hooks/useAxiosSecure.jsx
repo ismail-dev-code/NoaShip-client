c69b634c1da1846df16a17eef8bdfa86930bf698
ismail-dev-code <hm.ismail772@gmail.com>
2025-06-30 23:12:42 +0600

    feat: useAxiosSecure added  more functionality

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -1,38 +1,43 @@
-import { useEffect } from 'react';
 import axios from 'axios';
+import React from 'react';
 import useAuth from './useAuth';
+import { useNavigate } from 'react-router';
 
-
-// Create an Axios instance
 const axiosSecure = axios.create({
-  baseURL: 'http://localhost:5000',
+    baseURL: `http://localhost:5000`
 });
 
-// Custom hook to use secure Axios instance with auth token
 const useAxiosSecure = () => {
-  const { user } = useAuth();
+    const { user, logOut } = useAuth();
+    const navigate = useNavigate();
 
-  useEffect(() => {
-    const requestInterceptor = axiosSecure.interceptors.request.use(
-      (config) => {
-        // Add authorization header if user and token exist
-        if (user?.accessToken) {
-          config.headers.Authorization = `Bearer ${user.accessToken}`;
-        }
+    axiosSecure.interceptors.request.use(config => {
+        config.headers.Authorization = `Bearer ${user.accessToken}`
         return config;
-      },
-      (error) => {
+    }, error => {
         return Promise.reject(error);
-      }
-    );
+    })
 
-    // Eject interceptor on cleanup to prevent duplicate interceptors
-    return () => {
-      axiosSecure.interceptors.request.eject(requestInterceptor);
-    };
-  }, [user?.accessToken]);
+    axiosSecure.interceptors.response.use(res => {
+        return res;
+    }, error => {
+        const status = error.status;
+        if (status === 403) {
+            navigate('/forbidden');
+        }
+        else if (status === 401) {
+            logOut()
+                .then(() => {
+                    navigate('/login')
+                })
+                .catch(() => { })
+        }
 
-  return axiosSecure;
+        return Promise.reject(error);
+    })
+
+
+    return axiosSecure;
 };
 
 export default useAxiosSecure;
